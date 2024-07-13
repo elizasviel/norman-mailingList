e6980c05205cfc13d2a23ced1f267da6dfaded54
Norman Qian <normanqian@gmail.com>
2024-07-13 09:07:38 -0400

    more styling

--- frontend/src/BrowseLists.tsx
+++ frontend/src/BrowseLists.tsx
@@ -13,126 +13,204 @@ const BrowseLists = ({
       .then((res) => res.json())
       .then((data) => getAllMailingLists(data));
   }, []);
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        backgroundColor: "#432818",
-        padding: "10px",
-        width: "13vw",
-        border: "2px solid #FFE6A7",
-      }}
-    >
-      <h1 style={{ color: "#FFE6A7" }}> Email Lists</h1>
 
+  if (mailingLists.length === 0) {
+    return (
       <div
         style={{
-          border: "1px solid #858585",
-          borderRadius: "2px",
-
           display: "flex",
           flexDirection: "column",
-          overflowY: "scroll",
-          height: "69%",
-          backgroundColor: "#484848",
+          backgroundColor: "#432818",
+          padding: "10px",
+          width: "13vw",
+          border: "2px solid #FFE6A7",
         }}
       >
-        {mailingLists.map((list) => (
-          <div
-            key={list.id}
-            onClick={() => setSelectedList(list)}
-            className="listSelector"
+        <h1 style={{ color: "#FFE6A7" }}> Email Lists</h1>
+        <div
+          style={{
+            border: "1px solid #858585",
+            borderRadius: "2px",
+
+            display: "flex",
+            flexDirection: "column",
+            overflowY: "scroll",
+            height: "69%",
+            backgroundColor: "#484848",
+          }}
+        >
+          <h3 style={{ padding: "10px" }}>
+            No email lists in database.
+            <br></br>
+            <br></br>
+            Add a new email list to get started.
+          </h3>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            marginTop: "auto",
+            gap: "5px",
+          }}
+        >
+          <input
+            placeholder="Create New List"
+            type="text"
+            value={newListName}
+            onChange={(e) => setNewListName(e.target.value)}
             style={{
-              border: "0.1px solid #858585",
-              display: "flex",
-              flexDirection: "row",
               width: "100%",
-              height: "20px",
-              backgroundColor: "#BB9457",
-              color: "black",
-              alignItems: "center",
+            }}
+          />
+
+          <button
+            style={{
+              backgroundColor: "darkgreen",
+              marginLeft: "auto",
+              color: "#FFE6A7",
+            }}
+            onClick={async () => {
+              await fetch("http://localhost:3000/mailingLists", {
+                method: "POST",
+                body: JSON.stringify({ name: newListName }),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              });
+              fetch("http://localhost:3000/mailingLists")
+                .then((res) => res.json())
+                .then((data) => getAllMailingLists(data));
             }}
           >
-            <p style={{ paddingLeft: "5px" }}>{list.name}</p>
-            <button
-              style={{
-                color: "white",
-                marginLeft: "auto",
-                height: "20px",
-                borderRadius: "0px",
-              }}
-              onClick={() => {
-                //check if list empty
-                fetch(`http://localhost:3000/mailingLists/${list.id}`)
-                  .then((res) => res.json())
-                  .then((data) => {
-                    if (data.length === 0) {
-                      fetch(`http://localhost:3000/mailingLists/${list.id}`, {
-                        method: "DELETE",
-                      }).then(() => {
-                        fetch("http://localhost:3000/mailingLists")
-                          .then((res) => res.json())
-                          .then((data) => getAllMailingLists(data))
-                          .then(() => {
-                            setSelectedList(null);
-                          });
-                      });
-                    } else {
-                      alert("List not empty");
-                    }
-                  });
-              }}
-            >
-              x
-            </button>
-          </div>
-        ))}
+            +
+          </button>
+        </div>
       </div>
-
+    );
+  } else {
+    return (
       <div
         style={{
           display: "flex",
-          flexDirection: "row",
-          width: "100%",
-          marginTop: "auto",
-          gap: "5px",
+          flexDirection: "column",
+          backgroundColor: "#432818",
+          padding: "10px",
+          width: "13vw",
+          border: "2px solid #FFE6A7",
         }}
       >
-        <input
-          placeholder="Create New List"
-          type="text"
-          value={newListName}
-          onChange={(e) => setNewListName(e.target.value)}
-          style={{
-            width: "100%",
-          }}
-        />
+        <h1 style={{ color: "#FFE6A7" }}> Email Lists</h1>
 
-        <button
+        <div
           style={{
-            backgroundColor: "darkgreen",
-            marginLeft: "auto",
-            color: "#FFE6A7",
-          }}
-          onClick={async () => {
-            await fetch("http://localhost:3000/mailingLists", {
-              method: "POST",
-              body: JSON.stringify({ name: newListName }),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            });
-            fetch("http://localhost:3000/mailingLists")
-              .then((res) => res.json())
-              .then((data) => getAllMailingLists(data));
+            border: "1px solid #858585",
+            borderRadius: "2px",
+
+            display: "flex",
+            flexDirection: "column",
+            overflowY: "scroll",
+            height: "69%",
+            backgroundColor: "#484848",
           }}
         >
-          +
-        </button>
+          {mailingLists.map((list) => (
+            <div
+              key={list.id}
+              onClick={() => setSelectedList(list)}
+              className="listSelector"
+              style={{
+                border: "0.1px solid #858585",
+                display: "flex",
+                flexDirection: "row",
+                width: "100%",
+                height: "20px",
+                backgroundColor: "#BB9457",
+                color: "black",
+                alignItems: "center",
+              }}
+            >
+              <p style={{ paddingLeft: "5px" }}>{list.name}</p>
+              <button
+                style={{
+                  color: "white",
+                  marginLeft: "auto",
+                  height: "20px",
+                  borderRadius: "0px",
+                }}
+                onClick={() => {
+                  //check if list empty
+                  fetch(`http://localhost:3000/mailingLists/${list.id}`)
+                    .then((res) => res.json())
+                    .then((data) => {
+                      if (data.length === 0) {
+                        fetch(`http://localhost:3000/mailingLists/${list.id}`, {
+                          method: "DELETE",
+                        }).then(() => {
+                          fetch("http://localhost:3000/mailingLists")
+                            .then((res) => res.json())
+                            .then((data) => getAllMailingLists(data))
+                            .then(() => {
+                              setSelectedList(null);
+                            });
+                        });
+                      } else {
+                        alert("List not empty");
+                      }
+                    });
+                }}
+              >
+                x
+              </button>
+            </div>
+          ))}
+        </div>
+
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            marginTop: "auto",
+            gap: "5px",
+          }}
+        >
+          <input
+            placeholder="Create New List"
+            type="text"
+            value={newListName}
+            onChange={(e) => setNewListName(e.target.value)}
+            style={{
+              width: "100%",
+            }}
+          />
+
+          <button
+            style={{
+              backgroundColor: "darkgreen",
+              marginLeft: "auto",
+              color: "#FFE6A7",
+            }}
+            onClick={async () => {
+              await fetch("http://localhost:3000/mailingLists", {
+                method: "POST",
+                body: JSON.stringify({ name: newListName }),
+                headers: {
+                  "Content-Type": "application/json",
+                },
+              });
+              fetch("http://localhost:3000/mailingLists")
+                .then((res) => res.json())
+                .then((data) => getAllMailingLists(data));
+            }}
+          >
+            +
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default BrowseLists;
