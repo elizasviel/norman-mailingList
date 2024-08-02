9aa22b1024e9b65ec0cc41f49d0a7494f8406fbe
Norman Qian <normanqian@gmail.com>
2024-08-02 19:53:15 -0400

    add key back

--- backend/mailgun.ts
+++ backend/mailgun.ts
@@ -4,7 +4,9 @@ import Mailgun from "mailgun.js";
 const mailgun = new Mailgun(formData);
 const mg = mailgun.client({
   username: "api",
-  key: process.env.MAILGUN_API_KEY || "",
+  key:
+    process.env.MAILGUN_API_KEY ||
+    "6be1f6fd29c60b6b3fe333975be9d052-8a084751-4aa0e14a",
 });
 
 const sendEmail = (to: string, subject: string, body: string) => {
